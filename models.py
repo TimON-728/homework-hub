3fbd51f96ef54edd4fd5f91093a06f9382b11097
Timofey Andronov <timoxa.0510111@gmail.com>
2026-09-02 22:31:34 +0300

    Добавил город, школу и класс в базу данных, добавил валидацию данных регистрации

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-from sqlalchemy import Column, Integer, String, DateTime, JSON
+from sqlalchemy import Column, Integer, String, JSON, UniqueConstraint
 from sqlalchemy.sql import func
 
 engine = create_engine("sqlite:///./homework.db")
@@ -12,12 +12,18 @@ Base = declarative_base()
 class Homework(Base):
     __tablename__ = "homework"
     id = Column(Integer, primary_key=True)
+
     subject = Column(String)
     description = Column(String)
     photos = Column(JSON)
 
-Base.metadata.create_all(engine)
+    city = Column(String, nullable=False)
+    school = Column(String, nullable=False)
+    class_name = Column(String, nullable=False)
 
-db = SessionLocal()
-hw = db.query(Homework).first()  
-db.close()  
+    __table_args__ = (
+        UniqueConstraint('city', 'school', 'class_name', 'subject', name='unique_subject'),
+        UniqueConstraint('city', 'school', 'class_name', name='unique_class'),
+    )
+
+Base.metadata.create_all(engine)
--- validation.py
+++ validation.py
@@ -17,3 +17,16 @@ class HomeworkCreate(BaseModel):
         if not self.task.strip() and not self.photo:
             raise ValueError('Задание и фото не могут быть одновременно пустыми')
         return self
+
+
+class Registration(BaseModel):
+    city: str
+    school: str
+    class_name: str
+
+    @field_validator('city', 'school', 'class_name')
+    @classmethod
+    def not_empty(cls, v: str) -> str:
+        if not v.strip():
+            raise('Город, школа или класс не могут быть пустыми')
+        return v.strip()
